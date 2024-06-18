347f94e4a95cef407db1cf68e5f2ee42c4a3c90d
Akash Shukla <akashukla0341@gmail.com>
2024-06-18 20:34:57 +0530

    Initial Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,92 @@
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { logout, setOnlineUser, setSocketConnection, setUser } from '../redux/userSlice'
+import Sidebar from '../components/Sidebar'
+import logo from '../assets/logo.png'
+import io from 'socket.io-client'
+import Cookies from 'js-cookie';
+
+const Home = () => {
+  const user = useSelector(state => state.user)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const fetchUserDetails = async()=>{
+    try {
+      let token = localStorage.getItem('token')
+      const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`;
+      const response = await axios({
+        url: URL,
+        method: 'GET',
+        headers: {
+          'Authorization': token
+        },
+        withCredentials: true // This ensures that cookies are sent with the request if needed
+      });
+        dispatch(setUser(response.data.data))
+        if(response.data.data.logout){
+          Cookies.remove("token");
+          localStorage.clear()
+            dispatch(logout())
+            navigate("/email")
+        }
+    } catch (error) {
+        console.log("error",error)
+    }
+  }
+
+  useEffect(()=>{
+    fetchUserDetails()
+  },[])
+
+  /***socket connection */
+  useEffect(()=>{
+    const socketConnection = io(process.env.REACT_APP_BACKEND_URL,{
+      auth : {
+        token : localStorage.getItem('token')
+      },
+    })
+
+    socketConnection.on('onlineUser',(data)=>{
+      dispatch(setOnlineUser(data))
+    })
+
+    dispatch(setSocketConnection(socketConnection))
+
+    return ()=>{
+      socketConnection.disconnect()
+    }
+  },[])
+
+
+  const basePath = location.pathname === '/'
+  return (
+    <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
+        <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
+           <Sidebar/>
+        </section>
+
+        {/**message component**/}
+        <section className={`${basePath && "hidden"}`} >
+            <Outlet/>
+        </section>
+
+
+        <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex" }`}>
+            <div>
+              <img
+                src={logo}
+                width={250}
+                alt='logo'
+              />
+            </div>
+            <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+        </div>
+    </div>
+  )
+}
+
+export default Home
